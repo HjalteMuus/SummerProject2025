d154ba9ae797d9c69b889cbc4d11e1763d505391
HjalteMuus <hjaltemb2@gmail.com>
2025-06-02 19:10:07 +0200

    Added UI and controller class, added Enum Type

---
+++ src/Pokedex/Type.java
@@ -0,0 +1,33 @@
+package Pokedex;
+
+public enum Type {
+    WATER("Water"),
+    FIRE("Fire"),
+    GRASS("Grass"),
+    ELECTRIC("Electric"),
+    GROUND("Ground"),
+    ROCK("Rock"),
+    DRAGON("Dragon"),
+    ICE("Ice"),
+    PSYCHIC("Psychic"),
+    DARK("Dark"),
+    BUG("Bug"),
+    POISON("Poison"),
+    FIGHTING("Fighting"),
+    FLYING("Flying"),
+    NORMAL("Normal"),
+    FAIRY("Fairy"),
+    GHOST("Ghost"),
+    STEEL("Steel"),
+    ;
+
+    private final String typeName;
+
+    Type(String typeName){
+        this.typeName = typeName;
+    }
+
+    public String getTypeName(){
+        return typeName;
+    }
+}
