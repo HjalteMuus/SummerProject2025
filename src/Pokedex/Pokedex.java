90f41a1471101759f159b9c1a482d22873b5e8c1
HjalteMuus <hjaltemb2@gmail.com>
2025-06-16 15:14:53 +0200

    Some progress on loading, need to find a format for the csv file

--- src/Pokedex/Pokedex.java
+++ src/Pokedex/Pokedex.java
@@ -1,8 +1,35 @@
 package Pokedex;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Pokedex {
-    ArrayList<Pokemon> playerPokedex = new ArrayList<>();
-    ArrayList<Pokemon> notDiscoveredPokedex = new ArrayList<>();
+    private final ArrayList<Pokemon> playerPokedex;
+    private final ArrayList<Pokemon> notDiscoveredPokedex;
+    private final String path;
+
+    public Pokedex(String path){
+        this.playerPokedex = new ArrayList<>();
+        this.notDiscoveredPokedex = new ArrayList<>();
+        this.path = path;
+    }
+
+    private void loadPlayerPokedex(String path){
+        File csvFile = new File(path);
+        int lineCounter = 1;
+
+        try(Scanner reader = new Scanner(csvFile)){
+            reader.nextLine();
+            while(reader.hasNextLine()){
+                lineCounter++;
+            }
+        }catch (FileNotFoundException e){
+            throw new RuntimeException("File not found: " + path);
+        } catch (Exception e) {
+            throw new RuntimeException("Incompatible file: error at line " + lineCounter + " in file: " + csvFile.getAbsolutePath());
+        }
+    }
+
 }
