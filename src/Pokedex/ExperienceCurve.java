a93b57c7064aa58abf2869a05277b402c13f0781
HjalteMuus <hjaltemb2@gmail.com>
2025-06-16 14:43:36 +0200

    Medium exp curve split into medium fast and medium slow

--- src/Pokedex/ExperienceCurve.java
+++ src/Pokedex/ExperienceCurve.java
@@ -2,7 +2,8 @@ package Pokedex;
 
 public enum ExperienceCurve {
     SLOW(0,"Slow"),
-    MEDIUM(0,"Medium"),
+    MEDIUM_FAST(0,"Medium Fast"),
+    MEDIUM_SLOW(0, "Medium Slow"),
     FAST(0,"Fast"),
     ;
 
@@ -25,10 +26,16 @@ public enum ExperienceCurve {
     public void setTotalExpToNextLvl(int lvl, ExperienceCurve e){
         if(e == SLOW){
             totalExpToNextLvl = (int)(Math.ceil(Math.pow(lvl+1,3)*1.25-Math.pow(lvl,3)*1.25));
-        }else if(e == MEDIUM){
+        }else if(e == MEDIUM_FAST){
             totalExpToNextLvl = (int)(Math.ceil(Math.pow(lvl+1,3))-Math.pow(lvl,3));
-        }else{
+        }else if(e == FAST){
             totalExpToNextLvl = (int)(Math.ceil(Math.pow(lvl+1,3)*0.8-Math.pow(lvl,3)*0.8));
+        }else{
+            if(lvl == 1){
+                totalExpToNextLvl = 9;
+                return;
+            }
+            totalExpToNextLvl = (int)(Math.ceil(1.2*Math.pow(lvl+1,3)-15*Math.pow(lvl+1,2)+100*(lvl+1)-140)-(1.2*Math.pow(lvl,3)-15*Math.pow(lvl,2)+100*(lvl)-140));
         }
         if (lvl == 100) totalExpToNextLvl = 0;
     }
--- src/PokemonGame.java
+++ src/PokemonGame.java
@@ -5,12 +5,14 @@ import static Pokedex.Type.*;
 
 public class PokemonGame {
     public static void main(String[] args) {
-        Pokemon pokemon = new Pokemon("Bulbasaur", GRASS, POISON, 1, 0, SLOW);
-        Pokemon p = new Pokemon("Squirtle", WATER, 1, 0, MEDIUM);
-        Pokemon p2 = new Pokemon("Charmander", FIRE, 1, 0, FAST);
+        Pokemon pokemon = new Pokemon("Bulbasaur", GRASS, POISON, 20, 0, SLOW);
+        Pokemon p = new Pokemon("Squirtle", WATER, 20, 0, MEDIUM_SLOW);
+        Pokemon p2 = new Pokemon("Charmander", FIRE, 20, 0, FAST);
+        Pokemon p3 = new Pokemon("Weedle", BUG, POISON, 20, 0, MEDIUM_FAST);
 
         System.out.println(pokemon);
         System.out.println(p);
         System.out.println(p2);
+        System.out.println(p3);
     }
 }
